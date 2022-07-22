a1e996972659b7b7a643e521151d49c7766cb25f
him7emh1gh <109800359+him7emh1gh@users.noreply.github.com>
2022-07-22 16:44:55 +0300

    Upd fix test

--- tests/test_application.py
+++ tests/test_application.py
@@ -1,6 +1,6 @@
 from flask_posts.models import UserModel
 
 def test_new_user():
-  user = User("qwa@mail.ru", "password")
+  user = UserModel("qwa@mail.ru", "password")
   assert user.email == "qwa@mail.ru"
   assert user.token != "password"
